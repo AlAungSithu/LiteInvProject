013667f4aed0e689ed512d214a0040dde7d76fa3
Sithu <saung@purdue.edu>
2020-10-13 03:17:26 -0400

    Current deployment: Changes .body to .query

--- LiteInvBackend/functions/index.js
+++ LiteInvBackend/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 // const express = require('express');
-// const cors = require('cors');
+const cors = require('cors')({origin: true});
 const mysql = require('mysql');
 // const { extractInstanceAndPath } = require('firebase-functions/lib/providers/database');
 var admin = require('firebase-admin');
@@ -17,119 +17,135 @@ admin.initializeApp();
 
 // SELLER FUNCTIONS
 exports.create_seller = functions.https.onRequest(async (request, response) => {
-    /* FORMAT
-        "Success": true/false,
-        "SellerId": inserted id     OR      -1 on Success == false,
-        "SellerName": "name sent to backend",
-        "SellerEmail": "email sent to backend"
-    */
-    let name = request.body.seller_name;
-    let email = request.body.seller_email;
-    res = [];
+    cors(request, response, () => {
+    
+        /* FORMAT
+            "Success": true/false,
+            "SellerId": inserted id     OR      -1 on Success == false,
+            "SellerName": "name sent to backend",
+            "SellerEmail": "email sent to backend"
+        */
+        let name = request.query.seller_name;
+        let email = request.query.seller_email;
+        res = [];
 
-    con.query(`INSERT INTO Seller (SellerName, SellerEmail) VALUES ("${name}", "${email}");`, (err, rows, fields) => {
-        if (!err) {
-            res.push({Success: true, SellerId: rows.insertId, SellerName: name, SellerEmail: email});
-            response.send(res);
-        } else {
-            res.push({Success: false, SellerId: -1, SellerName: name, SellerEmail: email});
-            response.send(res);
-        }
+        con.query(`INSERT INTO Seller (SellerName, SellerEmail) VALUES ("${name}", "${email}");`, (err, rows, fields) => {
+            if (!err) {
+                res.push({Success: true, SellerId: rows.insertId, SellerName: name, SellerEmail: email});
+                response.send(res);
+            } else {
+                res.push({Success: false, SellerId: -1, SellerName: name, SellerEmail: email});
+                response.send(res);
+            }
+        })
+
     })
 });
 
 exports.retrieve_seller = functions.https.onRequest(async (request, response) => {
-    /* FORMAT
-        ON SUCCESS - person with that id found
-            "SellerId": retrieved-id,
-            "SellerName": "retrieved-name",
-            "SellerEmail": "retrieved-email"
-        ON FAILURE - no person with that id found
-            Blank
-    */
-    let id = request.body.seller_id;
-    if (id) {
-        // ID Passed, Return Specific
-        con.query(`SELECT * FROM Seller WHERE SellerId = ${id}`, (err, rows, fields) => {
-            if (!err)
-                response.send(rows);
-            else
-                response.send();
-        })
-    } else {
-        // No ID Passed, Return All
-        con.query('SELECT * FROM Seller', (err, rows, fields) => {
-            if (!err)
-                response.send(rows);
-            else
-                response.send();
-        })
-    }
+    cors(request, response, () => {
+    
+        /* FORMAT
+            ON SUCCESS - person with that id found
+                "SellerId": retrieved-id,
+                "SellerName": "retrieved-name",
+                "SellerEmail": "retrieved-email"
+            ON FAILURE - no person with that id found
+                Blank
+        */
+        let id = request.query.seller_id;
+        if (id) {
+            // ID Passed, Return Specific
+            con.query(`SELECT * FROM Seller WHERE SellerId = ${id}`, (err, rows, fields) => {
+                if (!err)
+                    response.send(rows);
+                else
+                    response.send();
+            })
+        } else {
+            // No ID Passed, Return All
+            con.query('SELECT * FROM Seller', (err, rows, fields) => {
+                if (!err)
+                    response.send(rows);
+                else
+                    response.send();
+            })
+        }
+
+    })
 });
 
 
 exports.update_seller = functions.https.onRequest(async (request, response) => {
-    /* FORMAT
-        "Success": true/false
-    */
-    let id = request.body.seller_id;
-    let new_name = request.body.seller_name;
-    let new_email = request.body.seller_email;
-    let sql;
-    res = [];
-    if (new_name && new_email) {
-        sql = `UPDATE Seller SET SellerName = "${new_name}", SellerEmail = "${new_email}" WHERE SellerId = "${id}";`
-    } else if (new_name) {
-        sql = `UPDATE Seller SET SellerName = "${new_name}" WHERE SellerId = "${id}";`
-    } else if (new_email) {
-        sql = `UPDATE Seller SET SellerEmail = "${new_email}" WHERE SellerId = "${id}";`
-    } else {
-        // Neither input
-        res.push({Success: false})
-        response.send(res);
-    }
+    cors(request, response, () => {
 
-    con.query(sql, (err, rows, fields) => {
-        if (!err) {
-            res = [];
-            res.push({Success: true})
-            response.send(res);
+        /* FORMAT
+            "Success": true/false
+        */
+        let id = request.query.seller_id;
+        let new_name = request.query.seller_name;
+        let new_email = request.query.seller_email;
+        let sql;
+        res = [];
+        if (new_name && new_email) {
+            sql = `UPDATE Seller SET SellerName = "${new_name}", SellerEmail = "${new_email}" WHERE SellerId = "${id}";`
+        } else if (new_name) {
+            sql = `UPDATE Seller SET SellerName = "${new_name}" WHERE SellerId = "${id}";`
+        } else if (new_email) {
+            sql = `UPDATE Seller SET SellerEmail = "${new_email}" WHERE SellerId = "${id}";`
         } else {
-            res = [];
+            // Neither input
             res.push({Success: false})
             response.send(res);
         }
+
+        con.query(sql, (err, rows, fields) => {
+            if (!err) {
+                res = [];
+                res.push({Success: true})
+                response.send(res);
+            } else {
+                res = [];
+                res.push({Success: false})
+                response.send(res);
+            }
+        })
+
     })
 });
 
 
 exports.delete_seller = functions.https.onRequest(async (request, response) => {
-    /* FORMAT
-        "Success": true/false
-    */
-    let id = request.body.seller_id;
     
-    if (!id) {
-        res = [];
-        res.push({Success: false})
-        response.send(res);
-    }
-    
-    con.query(`DELETE FROM Seller WHERE SellerId = ${id}`, (err, rows, fields) => {
-        affectedRows = rows.affectedRows;
-        if (!err) {
+    cors(request, response, () => {
+        /* FORMAT
+            "Success": true/false
+        */
+        let id = request.query.seller_id;
+        
+        if (!id) {
             res = [];
-            if (affectedRows) {
-                res.push({Success: true})
-            } else {
-                res.push({Success: false})
-            }
-            response.send(res);
-        }
-        else
             res.push({Success: false})
             response.send(res);
+        }
+        
+        con.query(`DELETE FROM Seller WHERE SellerId = ${id}`, (err, rows, fields) => {
+            affectedRows = rows.affectedRows;
+            if (!err) {
+                res = [];
+                if (affectedRows) {
+                    res.push({Success: true})
+                } else {
+                    res.push({Success: false})
+                }
+                response.send(res);
+            }
+            else
+                res.push({Success: false})
+                response.send(res);
+        })
     })
+    
 });
 
 
